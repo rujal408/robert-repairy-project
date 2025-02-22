4e116c4bbbdca08c6a86e57ae1ddf837864ca4ee
rujal408 <rujal408@gmail.com>
2025-02-22 09:12:44 +0545

    setup wireframe for settings page

--- src/app/(dashboard)/settings/page.jsx
+++ src/app/(dashboard)/settings/page.jsx
@@ -1,7 +1,17 @@
-import React from "react";
-
 const Settings = () => {
-  return <div>Settings</div>;
+  return (
+    <div className="ml-1 h-full">
+      <div className="fixed p-3 bg-white w-[200px] h-[100vh] top-0">
+        <span className="font-semibold">Settings</span>
+      </div>
+      <div className="relative bg-white p-3 h-full left-[210px] w-[calc(100%-210px)]">
+        <div className="flex gap-2 flex-col">
+          <h1>Profile</h1>
+          <div>Form</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Settings;
---
+++ src/app/api/booking/route.js
@@ -0,0 +1,115 @@
+import { NextResponse } from "next/server";
+
+export const GET = () => {
+  const data = [
+    {
+      date: "2025-02-05",
+      booking_name: "Shop",
+    },
+    {
+      date: "2025-02-05",
+      booking_name: "Gym",
+    },
+    {
+      date: "2025-02-06",
+      booking_name: "Library",
+    },
+    {
+      date: "2025-02-07",
+      booking_name: "Restaurant",
+    },
+    {
+      date: "2025-02-10",
+      booking_name: "Theater",
+    },
+    {
+      date: "2025-02-11",
+      booking_name: "Mall",
+    },
+    {
+      date: "2025-02-11",
+      booking_name: "Cinema",
+    },
+    {
+      date: "2025-02-12",
+      booking_name: "Cafe",
+    },
+    {
+      date: "2025-02-12",
+      booking_name: "Hotel",
+    },
+    {
+      date: "2025-02-12",
+      booking_name: "Coworking Space",
+    },
+    {
+      date: "2025-02-14",
+      booking_name: "Resort",
+    },
+    {
+      date: "2025-02-18",
+      booking_name: "Conference Room",
+    },
+    {
+      date: "2025-02-20",
+      booking_name: "Salon",
+    },
+    {
+      date: "2025-02-25",
+      booking_name: "Stadium",
+    },
+    {
+      date: "2025-03-01",
+      booking_name: "Park",
+    },
+    {
+      date: "2025-03-03",
+      booking_name: "Music Studio",
+    },
+    {
+      date: "2025-03-05",
+      booking_name: "Dance Hall",
+    },
+    {
+      date: "2025-03-07",
+      booking_name: "Resort",
+    },
+    {
+      date: "2025-03-10",
+      booking_name: "Exhibition Center",
+    },
+    {
+      date: "2025-03-12",
+      booking_name: "Coaching Center",
+    },
+    {
+      date: "2025-03-15",
+      booking_name: "Event Hall",
+    },
+    {
+      date: "2025-03-18",
+      booking_name: "Bowling Alley",
+    },
+    {
+      date: "2025-03-20",
+      booking_name: "Gaming Arena",
+    },
+    {
+      date: "2025-03-22",
+      booking_name: "Lounge",
+    },
+    {
+      date: "2025-03-25",
+      booking_name: "Office Space",
+    },
+    {
+      date: "2025-03-28",
+      booking_name: "Spa",
+    },
+    {
+      date: "2025-03-30",
+      booking_name: "Banquet Hall",
+    },
+  ];
+  return NextResponse.json({ data });
+};
