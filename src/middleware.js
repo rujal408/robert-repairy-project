32ff21354961997a22a325de440a3357659946a1
rujal408 <rujal408@gmail.com>
2025-02-22 13:15:13 +0545

    add middleware to the route

---
+++ src/middleware.js
@@ -0,0 +1,23 @@
+import { NextResponse } from "next/server";
+import { auth } from "./lib/auth";
+
+const authenticatedRoutes = ["/settings", "/analytics", "/calendar"];
+
+// This function can be marked `async` if using `await` inside
+export async function middleware(request) {
+  const authenticated = await auth();
+  const pathname = request?.nextUrl?.pathname;
+  if (authenticatedRoutes.includes(pathname)) {
+    if (authenticated) {
+      return NextResponse.next();
+    } else {
+      return NextResponse.redirect(new URL("/", request.url));
+    }
+  }
+  return NextResponse.next();
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: ["/", "/settings/:path*", "/analytics", "/calendar"],
+};
